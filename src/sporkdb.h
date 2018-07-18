05f11bdc04cfa9c4541ed8daad6bd01ed4dc29eb
hmgujarati <41371034+hmgujarati@users.noreply.github.com>
2018-07-18 18:35:56 +0530

    Update sporkdb.h

--- src/sporkdb.h
+++ src/sporkdb.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef PAXEXCOIN_CSPORKDB_H
-#define PAXEXCOIN_CSPORKDB_H
+#ifndef RSCoin_CSPORKDB_H
+#define RSCoin_CSPORKDB_H
 
 #include <boost/filesystem/path.hpp>
 #include "leveldbwrapper.h"
@@ -25,4 +25,4 @@ public:
 };
 
 
-#endif //PAXEXCOIN_CSPORKDB_H
+#endif //RSCoin_CSPORKDB_H

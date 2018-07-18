6c0982d9c1c16b9fbbd4f33c15276486537bec86
hmgujarati <41371034+hmgujarati@users.noreply.github.com>
2018-07-18 18:28:23 +0530

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -6,7 +6,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/paxchange-config.h"
+#include "config/rscoin-config.h"
 #endif
 
 #include "util.h"
@@ -105,7 +105,7 @@ std::string to_internal(const std::string&);
 
 using namespace std;
 
-// PAXCHANGE only features
+// RSCoin only features
 // Masternode
 bool fMasterNode = false;
 string strMasterNodePrivKey = "";
@@ -227,8 +227,8 @@ bool LogAcceptCategory(const char* category)
             const vector<string>& categories = mapMultiArgs["-debug"];
             ptrCategory.reset(new set<string>(categories.begin(), categories.end()));
             // thread_specific_ptr automatically deletes the set when the thread ends.
-            // "PAXCHANGE" is a composite category enabling all PAXCHANGE-related debug output
-            if (ptrCategory->count(string("PAXCHANGE"))) {
+            // "RSCoin" is a composite category enabling all RSCoin-related debug output
+            if (ptrCategory->count(string("RSCoin"))) {
                 ptrCategory->insert(string("swifttx"));
                 ptrCategory->insert(string("masternode"));
                 ptrCategory->insert(string("mnpayments"));
@@ -391,7 +391,7 @@ static std::string FormatException(std::exception* pex, const char* pszThread)
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "PAXCHANGE";
+    const char* pszModule = "RSCoin";
 #endif
     if (pex)
         return strprintf(
@@ -412,13 +412,13 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-// Windows < Vista: C:\Documents and Settings\Username\Application Data\PAXEXcoin
-// Windows >= Vista: C:\Users\Username\AppData\Roaming\PAXEXcoin
-// Mac: ~/Library/Application Support/PAXEXcoin
-// Unix: ~/.PAXCHANGE
+// Windows < Vista: C:\Documents and Settings\Username\Application Data\RSCoin
+// Windows >= Vista: C:\Users\Username\AppData\Roaming\RSCoin
+// Mac: ~/Library/Application Support/RSCoin
+// Unix: ~/.RSCoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "PAXEXcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "RSCoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -430,10 +430,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     TryCreateDirectory(pathRet);
-    return pathRet / "PAXEXcoin";
+    return pathRet / "RSCoin";
 #else
     // Unix
-    return pathRet / ".PAXCHANGE";
+    return pathRet / ".RSCoin";
 #endif
 #endif
 }
@@ -480,7 +480,7 @@ void ClearDatadirCache()
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "PAXCHANGE.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "RSCoin.conf"));
     if (!pathConfigFile.is_complete())
         pathConfigFile = GetDataDir(false) / pathConfigFile;
 
@@ -499,7 +499,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 {
     boost::filesystem::ifstream streamConfig(GetConfigFile());
     if (!streamConfig.good()) {
-        // Create empty PAXCHANGE.conf if it does not exist
+        // Create empty RSCoin.conf if it does not exist
         FILE* configFile = fopen(GetConfigFile().string().c_str(), "a");
         if (configFile != NULL)
             fclose(configFile);
@@ -510,7 +510,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
     setOptions.insert("*");
 
     for (boost::program_options::detail::config_file_iterator it(streamConfig, setOptions), end; it != end; ++it) {
-        // Don't overwrite existing settings so command line settings override PAXCHANGE.conf
+        // Don't overwrite existing settings so command line settings override RSCoin.conf
         string strKey = string("-") + it->string_key;
         string strValue = it->value[0];
         InterpretNegativeSetting(strKey, strValue);
@@ -525,7 +525,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 #ifndef WIN32
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "PAXCHANGEd.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "RSCoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }

9cfdb4e448edd79380d0091753b8e03f5c62899f
hmgujarati <41371034+hmgujarati@users.noreply.github.com>
2018-07-18 18:38:59 +0530

    Update spork.cpp

--- src/spork.cpp
+++ src/spork.cpp
@@ -27,7 +27,7 @@ CSporkManager sporkManager;
 std::map<uint256, CSporkMessage> mapSporks;
 std::map<int, CSporkMessage> mapSporksActive;
 
-// PAXEX: on startup load spork values from previous session if they exist in the sporkDB
+// RSC: on startup load spork values from previous session if they exist in the sporkDB
 void LoadSporksFromDB()
 {
     for (int i = SPORK_START; i <= SPORK_END; ++i) {
@@ -96,7 +96,7 @@ void ProcessSpork(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
         mapSporksActive[spork.nSporkID] = spork;
         sporkManager.Relay(spork);
 
-        // PAXEX: add to spork database.
+        // RSC: add to spork database.
         pSporkDB->WriteSpork(spork.nSporkID, spork);
     }
     if (strCommand == "getsporks") {
